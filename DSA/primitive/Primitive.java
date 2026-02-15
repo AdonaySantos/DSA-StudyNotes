1e0578741d04817eafff8c4211b9509209026c8d
AdonaySantos <adonay.rssantos@gmail.com>
2026-02-14 21:27:42 -0300

    primitive types

---
+++ DSA/primitive/Primitive.java
@@ -0,0 +1,12 @@
+public class Primitive {
+    // int, double, float, char, byte, short, long, boolean
+    byte byteAge = 10; // goes from -128 to 127 1 byte;
+    int age = 20; // int from -2.1 bi to 2.1 bi 4 bytes
+    long bigNumber = 100000000; // long from -2^63 to 2^63-1. 8 bytes
+    double temperature = 24.5; // double from 4.9e-324 to 1.7e+308 8 bytes
+    float floatSalary = 2500; // float from 1.4e-045 to 3.4e+038 4 bytes
+    boolean myTrue = true; // boolean from true or false 1 bit
+    boolean myFalse = false;
+    char letter = 'a'; // char from -32,768 to 32,767 2 bytes
+    String name = "Adonay";
+}
